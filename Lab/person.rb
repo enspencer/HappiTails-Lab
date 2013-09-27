57ec14c44a37f5b76090ec8af873d0066ad0f0ed
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-27 16:59:24 -0400

    Defining methods in classes

--- Lab/person.rb
+++ Lab/person.rb
@@ -1,11 +1,14 @@
 class Person
-	attr_accessor :name, :num_kids, :age, :num_pets, :pets
+	attr_accessor :name, :age, :num_pets, :pets
 
-	def initialize(name, num_kids, age, num_pets, pets)
+	def initialize(name, age, num_pets, pets)
 		@name = name
-		@num_kids = num_kids
 		@age = age
 		@num_pets = num_pets
 		@pets = {} #hash
 	end
+
+	#add or -essentially same fn as shelter, add animal into list
+	#remove pet from its list
+	#increment/decrement # of pets when you do that
 end
--- Lab/shelter.rb
+++ Lab/shelter.rb
@@ -2,8 +2,42 @@ class Shelter
 	attr_accessor :name, :address
 
 	def initialize(name, address)
-		@clients
-		@animals
+		@clients = []
+		@animals = []
 	end
-	
+
+	def add_pet(animal)
+		if animal.class == Animal
+			@animals << animal
+		else
+			return false
+		end
+	end
+
+	def add_client(person)
+		if client.class == Person
+			@clients << person
+		else
+			return false
+		end
+	end
+
+	def adopt_to(client, animal)
+	end
+
+	def return_pet
+
+	def animals
+	end
+
+#has_pet
+#adopt_to, pass client name and animal
+#accept
+#client doesn't have access to cats
+# ---
+#accept_animal (do we create list first?)
+#accept_client -add client obj to list
+#give pet to client > rel btwn client and shelter
+#accept pet from client > rel btwn client and shelter
+
 end
