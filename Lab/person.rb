f3046cd6085ede262ee1533807adb94503025d2f
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-27 17:08:24 -0400

    Defining methods

--- Lab/person.rb
+++ Lab/person.rb
@@ -8,7 +8,24 @@ class Person
 		@pets = {} #hash
 	end
 
+	def clients_pets
+	end
+
+	def add_pet
+	end
+
+	def remove_pet
+	end
+
 	#add or -essentially same fn as shelter, add animal into list
 	#remove pet from its list
 	#increment/decrement # of pets when you do that
 end
+
+# neither client nor shelter should have access to each other's pets list.
+#nowhere in accept pet fn here reference shelter
+
+# remove pet from shelter and adopt from client separately
+# 2 methods called when someone adopts
+
+# modularity and sustainability
